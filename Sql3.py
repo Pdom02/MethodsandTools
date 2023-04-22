90bd95ac0e3d7edfaaa1db1abf61addfdf7ea00a
TheoOfTroy <130273607+TheoOfTroy@users.noreply.github.com>
2023-04-22 15:55:41 -0500

    This one is for me to test the class thing for python and will probably work on it some more

---
+++ Sql3.py
@@ -0,0 +1,9 @@
+import sqlite3
+import getpass
+import os
+
+class User:
+        #constructor
+        def __init__ (self, UsrAge, UsrName);
+                self.UsrAge = UsrAge
+                self.UsrName = UsrName
