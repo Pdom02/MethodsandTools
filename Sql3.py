822edfaba5f178a695c0dd554a8d4c6c40d7c973
TheoOfTroy <130273607+TheoOfTroy@users.noreply.github.com>
2023-04-22 16:39:35 -0500

    Update Sql3.py

--- Sql3.py
+++ Sql3.py
@@ -1,9 +1,20 @@
 import sqlite3
 import getpass
+import random
 import os
+
+
+sqliteConnection = sqlite3.connect('User_datab.db')
 
 class User:
         #constructor
-        def __init__ (self, UsrAge, UsrName);
+        def __init__ (self, UsrAge, UsrName,address,UsrID):
                 self.UsrAge = UsrAge
                 self.UsrName = UsrName
+                self.address = address
+                self.UsrID =  UsrID
+
+
+        #setter = age
+        def setAge(self, UsrAge):
+                self.UsrAge = UsrAge
