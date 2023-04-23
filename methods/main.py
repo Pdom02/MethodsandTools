12bdcf0935cff34c7005495cd5e6cf737196d25b
brentts <123522289+brentts@users.noreply.github.com>
2023-04-23 14:15:37 -0500

    Added add listing and search listing function

--- methods/main.py
+++ methods/main.py
@@ -3,18 +3,23 @@ import getpass
 import os
 
 def clear():
-    os.system('cls')
+    os.system('clear')
 
 
 def main_loop():
     connection = sqlite3.connect("buyerschoice.db")
     cursor = connection.cursor()
-    print("Welcome to buyers choice!\n")
-    print("1. Login")
-    print("2. Register")
-    check1 = input("Type one of the numbers: ")
-    quit = 0
+    # print("Welcome to buyers choice!\n")
+    # print("1. Login")
+    # print("2. Register")
+    # check1 = input("Type one of the numbers: ")
+    # quit = 0
     while(1):
+        print("Welcome to buyers choice!\n")
+        print("1. Login")
+        print("2. Register")
+        check1 = input("Type one of the numbers: ")
+        quit = 0
         if check1 == "1":
             clear()
             username = input("Username: ")
@@ -27,8 +32,9 @@ def main_loop():
             while(quit != 1):
                 print("Welcome "  + username)
                 print("1. Update Profile content")
-                print("2. Search Listings")
-                print("3. Logout")
+                print("2, Add Listing")
+                print("3. Search Listings")
+                print("4. Logout")
                 check2 = input("Type one of the numbers: ")
                 if check2 == "1":
                     email = cursor.execute("SELECT email FROM Customer WHERE username = ? AND password = ?", (username, password))
@@ -37,9 +43,34 @@ def main_loop():
                     result2 = address.fetchone()
                     print("Current Username: " + username)
                     print("Password: ********")
-                    print(f"Adress: {result2!r}")
+                    print(f"Address: {result2!r}")
                     print(f"Email Address: {result!r} ")
+                if check2 == "2":
+                    itemName = input("Item Name: ")
+                    itemDesc = input("Itme Description: ")
+                    itemPrice = float(input("Price: $"))
+                    itemStock = int(input("Stock: "))
+                    itemCategory = input("Category: ")
+                    datapkg = (itemName, itemDesc, itemPrice, itemStock, itemCategory)
+                    cursor.execute("INSERT INTO Listings(itemName, itemDesc, itemPrice, itemStock, itemCategory) VALUES(?, ?, ?, ?, ?)", datapkg)
+                    connection.commit()
                 if check2 == "3":
+                    listingname = input("Search for an item listing: ")
+                    cursor.execute("SELECT * FROM Listings WHERE itemName = ?", (listingname, ))
+                    rows = cursor.fetchall()
+
+                    if len(rows) == 0:
+                        print("No listings found.")
+                    else:
+                        for row in rows:
+                            print()
+                            print("Name:", row[0])
+                            print("Description:", row[2])
+                            print("Price: ${:.2f}".format(row[3]))
+                            print("Stock:", row[4])
+                            print("Category:", row[5])
+                            print()
+                if check2 == "4":
                     break
 
         elif check1 == "2":
