0a904dc0dbca26174fa3c52734de976169ee6b2f
unknown <Padom02@gmail.com>
2023-04-29 16:05:27 -0500

    Finished merge problems and added admin functionality to the program

--- methods/main.py
+++ methods/main.py
@@ -4,6 +4,7 @@ import os
 from user_class import User
 from Item_class import Item
 from cart_class import ShoppingCart
+from store_class import Store
 
 def clear():
     if os.name == 'nt':
@@ -26,7 +27,7 @@ def create_table(conn, create_table_sql):
         print(e)
 
 def setup_db():
-    database = r"home\\brentts\\Documents\\GitHub\\MethodsandTools\\methods\\buyerschoice.db"
+    database = r"C:\\Users\\Phillip Dominguez\\Desktop\\methods_tools\\MethodsandTools\\methods\\buyerschoice.db"
 
     sql_create_customer_table = """ CREATE TABLE IF NOT EXISTS customer (
                                     firstname text,
@@ -80,6 +81,9 @@ def main_loop():
     setup_db()
     connection = sqlite3.connect("buyerschoice.db")
     cursor = connection.cursor()
+    adminID = "admin"
+    adminPass = "123admin"
+    adminguy = Store(adminID, adminPass)
     fella = User(connection)
     cart = ShoppingCart(connection, fella)
 
@@ -88,12 +92,12 @@ def main_loop():
         print("Welcome to buyers choice!\n")
         print("1. Login")
         print("2. Register")
-        print("3. Exit")
+        print("3. Admin Portal")
+        print("4. Exit")
         check1 = input("Type one of the numbers: ")
         if check1 == "1":
             clear()
             fella.login(cursor)
-            
             while(quit != 1):
                 print("Welcome "  + fella.getUsrname())
                 print("1. Update Profile content")
@@ -165,6 +169,67 @@ def main_loop():
         elif check1 == "2":
             fella.register(connection)
         elif check1 == "3":
+            username = input("Username: ")
+            password = getpass.getpass("Passowrd: ")
+            if (username == adminguy.getadminID()) & (password == adminguy.getadminPass()):
+                while(1):
+                    clear()
+                    print("Admin Panel")
+                    print("1. View Listings")
+                    print("2. View Accounts")
+                    print("3. Logout")
+                    admin_choice = input("Type the number of the option you want to select: ")
+                    if admin_choice == '1':
+                        cursor.execute("SELECT * FROM Listings")
+                        rows = cursor.fetchall()
+                        if len(rows) == 0:
+                            print("No listings found.")
+                            goback = input("Type 'b' to go back: ")
+                            if goback == 'b':
+                                continue
+                        else:
+                            for row in rows:
+                                print()
+                                print("Name:", row[0])
+                                print("ID: ", row[1])
+                                print("Description:", row[2])
+                                print("Price: ${:.2f}".format(row[3]))
+                                print("Stock:", row[4])
+                                print("Category:", row[5])
+                                print()
+                        rmv_choice = input("Type the itemID that you wish to remove, or type 'b' to go back: ")
+                        if rmv_choice == 'b':
+                            continue
+                        else:
+                            adminguy.removeListing(connection, cursor, rmv_choice)
+                    if admin_choice == '2':
+                        cursor.execute("SELECT * FROM Customer")
+                        rows = cursor.fetchall()
+                        if len(rows) == 0:
+                            print("No customers in DB")
+                            goback = input("Type 'b' to go back: ")
+                            if goback == 'b':
+                                continue
+                        else:
+                            for row in rows:
+                                print()
+                                print("Username: ", row[2])
+                                print("Full name: ", row[0], row[1])
+                                print("ID Num: ", row[5])
+                                print()
+                            customer_rmv = input("Type the ID Num for the account you wish to remove, or type 'b' to go back: ")
+                            if customer_rmv == 'b':
+                                continue
+                            else:
+                                adminguy.removeAccount(connection, cursor, customer_rmv)
+                    if admin_choice == '3':
+                        break
+                    
+            else:
+                print("Invalid credentials...")
+                continue
+        
+        elif check1 == "4":
             connection.close()
             exit()
 
--- methods/store_class.py
+++ methods/store_class.py
@@ -1,6 +1,39 @@
+import sqlite3
+from Item_class import Item
+
+
 
 class Store:
     
-    def __init__(self):
-        adminID = self.adminID
-        adminPass = self.adminPass
+    def __init__(self, adminID, adminPass):
+        self.adminID = adminID
+        self.adminPass = adminPass
+
+    def setadminID(self, adminUser):
+        self.adminID = adminUser
+    
+    def setadminPass(self, password):
+        self.adminPass = password
+    
+    def getadminID(self):
+        return self.adminID
+    
+    def getadminPass(self):
+        return self.adminPass
+    
+    def removeAccount(self, connection, cursor, customer_rmv):
+        cursor.execute("SELECT * FROM Customer")
+        rows = cursor.fetchall()
+        for row in rows:
+            if customer_rmv == str(row[5]):
+                cursor.execute("DELETE FROM Customer WHERE customerID=?", (row[5], ))
+                print("Item deleted")
+                connection.commit()
+    def removeListing(self, connection, cursor, rmv_choice):
+        cursor.execute("SELECT * FROM Listings")
+        rows = cursor.fetchall()
+        for row in rows:
+            if rmv_choice == str(row[1]):
+                cursor.execute("DELETE FROM Listings WHERE item_id=?", (row[1], ))
+                print("Item deleted")
+                connection.commit()
